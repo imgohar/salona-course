1e16a39be5f666c7cc6d14c0bfa61288b4dd0098
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2022-11-25 16:12:01 +0500

    fixes

--- airdrop/index.js
+++ airdrop/index.js
@@ -1,1 +1,11 @@
-const {Connection,PublicKey,clusterApiUrl,KeyPair,LAMPORTS_PER_SOL} = require("@solana/web3.js")
+const {Connection,PublicKey,clusterApiUrl,LAMPORTS_PER_SOL} = require("@solana/web3.js");
+
+const { Keypair } = require("@solana/web3.js");
+
+let keypair = Keypair.generate();
+
+const publicKey = new PublicKey(keypair._keypair.publicKey);
+const secretKey = keypair._keypair.secretKey;
+
+
+console.log(publicKey,secretKey)
